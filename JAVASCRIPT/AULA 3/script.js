8f66dda7b7f908cf1ba886ec791586b5bd2ba3f8
VSRohod <89645335+VSRohod@users.noreply.github.com>
2025-08-28 18:06:23 -0300

    aula 3

---
+++ JAVASCRIPT/AULA 3/script.js
@@ -0,0 +1,51 @@
+// var produto1 = "Jogo Midnight Club"
+// var produto2 = "Camiseta esportiva"
+// var produto3 = "Guitarra"
+
+// var produtos = Array();
+var produtos = [];
+console.log(produtos)
+
+produtos[0] = "Guitarra"
+produtos[1] = "Camiseta esportiva"
+produtos[2] = "Jogo Midnight Club"
+console.log(produtos)
+
+var carrinho = ["Tenis da adidas"]
+
+// comandos do array
+// add no fim
+carrinho.push("Violão")
+console.log(carrinho)
+// add no começo
+carrinho.unshift("Placa de video")
+console.log(carrinho)
+// remove no fim
+carrinho.pop()
+console.log(carrinho)
+// remove no começo
+carrinho.shift()
+console.log(carrinho)
+// mexe no meio (indice , quantos sobrescreve, dados)
+
+carrinho = []
+carrinho.push(numero1)
+carrinho.push("Relogio de couro")
+carrinho.push("Diamante do free fire")
+console.log(carrinho)
+
+
+
+carrinho.splice(2,0,"Robux")
+console.log(carrinho)
+carrinho.splice(4,1)
+console.log(carrinho)
+carrinho.splice(0,1,"Tenis da Nike")
+console.log(carrinho)
+
+
+
+
+
+
+
